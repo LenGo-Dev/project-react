0af8d70d45f18332ee94a8c20c7fcd4753cbd2aa
LenGo-Dev <lena.gotovska@gmail.com>
2025-11-14 00:22:35 +0100

    Fixed store file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 import initialState from './initialState';
-import { strContains } from '../utils/strContains';
 import listsReducer from './listsRedux';
 import cardsReducer from './cardsRedux';
 import columnsReducer from './columnsRedux';
 import searchStringReducer from './searchStringRedux';
-
-
-//selectors
-export const getFilteredCards = ({ cards, searchString }, columnId) => cards
-  .filter(card => card.columnId === columnId && strContains(card.title, searchString));
-
-// action creators
-export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
-export const addCard = payload => ({ type: 'ADD_CARD', payload });
 
 const subreducers = {
   lists: listsReducer,
